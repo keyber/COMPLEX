025c640c4580c60963a594642e02472e03fee712
keyber <ad.spif@gmail.com>
2018-12-05 11:52:15 +0100

    ex4 corrigé

--- premiers/ex4.py
+++ premiers/ex4.py
@@ -1,7 +1,6 @@
 import math
 import random
 
-
 def my_expo_mod(g, n, N):
     """n en représentation binaire (n[0] est le MSB)
     retourne g**n % N
@@ -34,11 +33,7 @@ assert my_gcd(10, 5) == 5
 assert my_gcd(10, 11) == 1
 
 
-def test_miller_rabin_BAD(n, T):
-    """n = 1 + 2**h * m
-    m sous la forme de sa représentatiton binaire
-    T nombre d'itération
-    retourne possible premier"""
+def test_miller_rabin(n, T):
     h = 0
     m = n - 1
     while m % 2 == 0:
@@ -49,65 +44,20 @@ def test_miller_rabin_BAD(n, T):
     for i in range(T):
         #a aléatoire inversible modulo n
         a = random.randint(2, n-1)
-        while my_gcd(a, n) != 1:
-            a = random.randint(2, n-1)
         
         #b = my_expo_mod(a, m, n)
         b = pow(a, m, n)
         
         if b != 1 and b != n - 1:
-            for _ in range(h-1):
-                if b != n - 1 and b*b % n == 1:
+            j=1
+            while j<h and b!=n-1:
+                b=b*b%n
+                if b == 1:
                     return 0
-                b = b*b%n
+                j+=1
             if b != n - 1:
                 return 0
     return 1
-
-
-def test_miller_rabin(n, T):
-    s = 0
-    d = n - 1
-    while d % 2 == 0:
-        d >>= 1
-        s += 1
-    assert (2 ** s * d == n - 1)
-    
-    def trial_composite(a):
-        if pow(a, d, n) == 1:
-            return False
-        for i in range(s):
-            if pow(a, 2 ** i * d, n) == n - 1:
-                return False
-        return True
-    
-    for _ in range(T):
-        a = random.randint(2, n-1)
-        if trial_composite(a):
-            return False
-    
-    return True
-
-for _i in range(5,1000,2):
-    if prime_test(_i):
-        assert test_miller_rabin(_i, 1)
-        
-#221 = 13*17 = 1 + 4 * 55 = 1 + 2**2 * 55
-#assert not test_miller_rabin(221, 2, [1, 1, 0, 1, 1, 1], 10)
-assert not test_miller_rabin(221, 10)
-
-#19 = 1 + 2**1 * 9   premier
-#assert test_miller_rabin(19, 1, [1, 0, 0, 1], 10)
-assert test_miller_rabin(19, 10)
-
-#561 = 3 * 11 * 17 est un nombre de carmichael
-#    = 1 + 2**4 * 35
-#assert not test_miller_rabin(561, 4, [1, 0, 0, 0, 1, 1], 10)
-assert not test_miller_rabin(561, 10)
-
-
-def error_proba(test, T):
-    return sum(test_miller_rabin(n, int(T)+(random.random()<T-int(T))) for n in test)/len(test)
 
 def getBinary(n):
     """retourne l'écriture en base 2 du nombre n.
@@ -136,16 +86,60 @@ def get_hm_formBinary(n):
     h,m = get_hm_form(n)
     return h, getBinary(m)
 
+
+"""def test_miller_rabin(n, T):
+    h, m = get_hm_formBinary(n)
+    
+    def strongTest(a):
+        b = my_expo_mod(a, m, n)
+        if b != 1 and b != n - 1:
+            for _ in range(h - 1):
+                b = b * b % n
+                if b == 1:
+                    return 0
+                if b == n - 1:
+                    return 1
+        return 1
+    
+    for _ in range(T):
+        a = random.randint(2, n-1)
+        if not strongTest(a):#on a trouvé un diviseur
+            return 0
+        
+    #on a jamais trouvé de diviseur
+    return 1"""
+
+for _i in range(5,1000,2):
+    if prime_test(_i):
+        assert test_miller_rabin(_i, 1)
+    
+#221 = 13*17 = 1 + 4 * 55 = 1 + 2**2 * 55
+assert not test_miller_rabin(221, 100)
+
+#19 = 1 + 2**1 * 9   premier
+#assert test_miller_rabin(19, 1, [1, 0, 0, 1], 10)
+assert test_miller_rabin(19, 10)
+
+#561 = 3 * 11 * 17 est un nombre de carmichael
+#    = 1 + 2**4 * 35
+#assert not test_miller_rabin(561, 4, [1, 0, 0, 0, 1, 1], 10)
+assert not test_miller_rabin(561, 100)
+
+
+def error_proba(test, T):
+    return sum(test_miller_rabin(n, T) for n in test)/len(test)
+
 def main():
     import matplotlib.pyplot as plt
-    s=0#10**10 #shift
-    composed = [i for i in range(s+1,s + 10**5,2) if not prime_test(i)]
+    #s=10**5 #shift
+    s=0 #shift
+    
+    composed = [i for i in range(s+1,s + 10**5 * 2,2) if not prime_test(i)]
     print("nombres composés considérés :", len(composed))
     
-    
-    r = [1+i*.2 for i in range(15)]
-    print(r)
-    err = [error_proba(composed, T) for T in r]
+    r = range(1,5)
+    redo=9
+    err = [sum(error_proba(composed, T) for _ in range(redo))/redo for T in r]
     errMin = max(min(err),1e-9)
     print([x/errMin*100 for x in err])
     plt.plot(r, err)
